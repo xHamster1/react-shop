c9465064796c87e73af410af1424ca59be385db1
xHamster1 <kirill540945@gmail.com>
2023-07-02 16:06:59 +0300

    Update application.service.ts

--- front-end/src/services/application.service.ts
+++ front-end/src/services/application.service.ts
@@ -1,10 +1,6 @@
-import { IApplication } from '@/store/application'
-
-import instance from '@/api/api.interceptor'
-
 const APPLICATION = 'application'
 
-export const sendMail = async data =>
+export const sendMail = async (data: any) =>
 	fetch('application', {
 		method: 'POST',
 		body: JSON.stringify(data),
